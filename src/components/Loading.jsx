9f7c2d5c4546ef6bb4ba7ce25767a185247316c7
hJava72 <harsonjava@yahoo.com>
2026-05-22 14:43:08 +0700

    dashboard page UI completed

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center h-screen items-center'>
+        <div className='animate-spin w-8 h-8 border-2 border-indigo-600 border-t-transparent rounded-full'></div>
+    </div>
+  )
+}
+
+export default Loading
